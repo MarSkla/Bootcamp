f26cda1ec23436d8b755dd8b86f0d511ecd8cfd4
MarSkla <marcin@skladanowski.net>
2021-10-28 14:38:30 +0200

    exercise_03.py - provided initial code, algo to be corrected

--- 04_python_introduction/exercise_03.py
+++ 04_python_introduction/exercise_03.py
@@ -1,2 +1,21 @@
 print('\nexercise_03.py:\n\tsort list elements in descending order')
+# Python has standard sorted() function and .sort() method (lists only)
+# however I will try to build my own function with same result as sorted()
 print()  # empty line for better readability
+
+
+def sorting(lst):
+    print(lst)
+    for idx in range(1, len(lst)):
+        for idx_next in range(idx - 1):
+            # print('idx: ', idx, 'type: ', type(idx))
+            # print('lst[idx - 1]: ', lst[idx - 1], 'type: ', type(lst[idx + 1]))
+            # print('lst[idx]: ', lst[idx], 'type: ', type(lst[idx]))
+            if lst[idx] < lst[idx - 1]:
+                lst[idx], lst[idx_next] = lst[idx_next], lst[idx]
+    print('lst sorted?: ', lst)
+
+
+x = [1, 3, 0, 2, 4, 5]
+
+sorting(x)
