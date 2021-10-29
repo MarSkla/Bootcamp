dd7adf0c658c9fc5688ba8f527ab1a1c07bff3aa
MarSkla <marcin@skladanowski.net>
2021-10-29 09:36:52 +0200

    exercise_03.py - option to sort in descending order added

--- 04_python_introduction/exercise_03.py
+++ 04_python_introduction/exercise_03.py
@@ -6,20 +6,27 @@ print('\nexercise_03.py:\n\tsort list elements in descending order')
 print()  # empty line for better readability
 
 
-def sorting(lst):
+def sorting(lst, direction='asc'):
     for first_index in range(len(lst)):
         for second_index in range(first_index):
-            if lst[second_index] > lst[first_index]:
-                lst[first_index], lst[second_index] = lst[second_index], \
-                                                      lst[first_index]
+            if direction == 'asc':
+                if lst[second_index] > lst[first_index]:
+                    lst[first_index], lst[second_index] = lst[second_index], \
+                                                          lst[first_index]
+            if direction == 'dsc':
+                if lst[second_index] < lst[first_index]:
+                    lst[first_index], lst[second_index] = lst[second_index], \
+                                                          lst[first_index]
     return lst
 
 
-test_case = [[1, 3, 0, 2, 4, 5], ['c', 'e', 'g', 'a', 'f'], ['c', 'E', 'g',
-                                                             'A', 'F']]
-test_no = 1
+# test_case = [[1, 3, -4, 2, 4, 5], ['c', 'e', 'g', 'a', 'f'], ['c', 'E', 'g',
+#                                                              'A', 'F']]
+# test_no = 1
+#
+# for case_lst in test_case:
+#     print(f'test no. {test_no} for list:\n\t {case_lst}')
+#     print('result:\n\t', sorting(case_lst), '\n')
+#     test_no += 1
 
-for case_lst in test_case:
-    print(f'test no. {test_no} for list:\n\t {case_lst}')
-    print('result:\n\t', sorting(case_lst), '\n')
-    test_no += 1
+print(sorting([1, 3, -4, 2, 4, 5], 'dsc'))
