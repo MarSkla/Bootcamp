f83f30d1477624dd145d20bdae1d2731fc92c2be
MarSkla <marcin@skladanowski.net>
2021-11-01 19:21:45 +0100

    exercise_05.py - added task info

---
+++ 04_python_introduction/exercise_05.py
@@ -0,0 +1,5 @@
+print('\nexercise_04.py:\n\tbuild a function that returns which birthdays '
+      'will be celebrated this year by those born in years privided in a '
+      'list being entry argument for that function')
+
+print()  # empty line for better readability
