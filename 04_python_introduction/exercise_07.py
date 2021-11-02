20c61666e2b854d8777e0b9928eb362fa3632ff8
MarSkla <marcin@skladanowski.net>
2021-11-02 15:11:27 +0100

    exercise_07.py - added working code

--- 04_python_introduction/exercise_07.py
+++ 04_python_introduction/exercise_07.py
@@ -1,3 +1,27 @@
 print('\nexercise_07.py:\n\tbuild function that count letters and numeric '
       'characters in a given string')
 print()  # empty line for better readability
+
+test_phrase = '1klaw$5 ~gr(:t'
+
+
+def count_char(str):
+    nums = 0
+    letters = 0
+    spaces = 0
+    others = 0
+    for element in str:
+        if element.isnumeric():
+            nums += 1
+        elif element.isalpha():
+            letters += 1
+        elif element.isspace():
+            spaces += 1
+        else:
+            others += 1
+
+    print(f'string length: {len(str)} \n\tnums: {nums} \n\tletters: {letters}'
+          f' \n\tspaces: {spaces} \n\tothers: {others}')
+
+
+count_char(test_phrase)
