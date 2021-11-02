859bbb4de8a7c1fbdeb9c3fe302e4b650c760776
MarSkla <marcin@skladanowski.net>
2021-11-02 10:53:02 +0100

    exercise_05.py - possibility to define year for anniversary check added to function

--- 04_python_introduction/exercise_05.py
+++ 04_python_introduction/exercise_05.py
@@ -1,15 +1,17 @@
 print('\nexercise_04.py:\n\tbuild a function that returns which birthdays '
       'will be celebrated this year by those born in years provided in list '
       'being entry argument for that function')
-
 print()  # empty line for better readability
 
+from datetime import datetime  # import left here with full premeditation
 
-def anniversary(lst):
-    current_year = int(input("Provide current year: "))
+current_year = datetime.today().year
+
+
+def anniversary(lst, check_date=current_year):
     for element in lst:
         print(f'Check for person born in {element}:')
-        anniversary_years = current_year - element
+        anniversary_years = check_date - element
         ending_dict = {1: 'st', 2: 'nd', 3: 'rd', 4: 'th'}
         for key in ending_dict.keys():
             if key == anniversary_years:
@@ -20,11 +22,13 @@ def anniversary(lst):
                 print(f'\tThis person is going to celebrate '
                       f'{anniversary_years}{ending_dict[4]} anniversary')
                 break
+            elif anniversary_years == 0:
+                print(f'\t First birthday will be celebrated next year')
+                break
         else:
             print(f'\tError - The person reported to be born in {element}'
                   f' has not been born yet')
-            break
 
 
 lst_a = [2020, 2019, 2018, 2017, 1993, 1970, 2013, 2032, 2021]
-anniversary(lst_a)
+anniversary(lst_a, 2019)
