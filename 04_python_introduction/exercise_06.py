479f6363f4ed023e55983134d777a25f71d9f1be
MarSkla <marcin@skladanowski.net>
2021-11-02 12:17:31 +0100

    exercise_06.py - created new file, task info included

---
+++ 04_python_introduction/exercise_06.py
@@ -0,0 +1,6 @@
+print('\nexercise_06.py:\n\ttake several countries and create separate '
+      'dictionaries for each of them (name, area, population, GDP) and build '
+      'a function able to sort data (list of all dictionaries) on different '
+      'key')
+print()  # empty line for better readability
+
