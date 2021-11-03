49a4792bda3e0aa8da025cd96ac5eb53deda81d4
MarSkla <marcin@skladanowski.net>
2021-11-03 11:08:07 +0100

    exercise_08.py - add working code

--- 04_python_introduction/exercise_08.py
+++ 04_python_introduction/exercise_08.py
@@ -1,3 +1,15 @@
-print('\nexercise_08.py:\n\tbuild function that check if its two arguments '
-      'are palindromes')
+print('\nexercise_08.py:\n\tbuild function that check if two strings being '
+      'its arguments are palindromes')
 print()  # empty line for better readability
+
+
+def palindromes(array_1, array_2):
+    assert isinstance(array_1, str) and isinstance(array_2, str), \
+        'both arguments have to be strings'
+    if array_1.lower()[::-1] == array_2.lower():
+        print('They are palindromes')
+    else:
+        print('They are not palindromes')
+
+
+palindromes('test', 'Tset')
