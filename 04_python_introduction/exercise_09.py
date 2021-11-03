35c966cc4d49fed30614801440c60cadd0fc8491
MarSkla <marcin@skladanowski.net>
2021-11-03 18:00:25 +0100

    modify working code to final version

--- 04_python_introduction/exercise_09.py
+++ 04_python_introduction/exercise_09.py
@@ -1,29 +1,30 @@
-print('\nexercise_09.py:\n\tuse nested loops to build function printing:',
+print('\nexercise_09.py:\n\tuse nested loops to build function printing ('
+      'arrow width = 5):',
       '*', '* $', '* $ *', '* $ * $', '* $ * $ *', '* $ * $', '* $ *', '* $',
       '*', sep='\n\t')
 print()  # empty line for better readability
 
+
 print('result:\n')
 
+
 def arrow(size):
-    lst = ''
-    half_size = (size // 2)
-    # print(f'half_size: {half_size}')
+    lst = '\t'
     counter = 0
     downsizer = -2
     while counter < size:
-        for num in range(1, half_size + 1):
+        for num in range(1, size + 1):
             if num % 2 == 1:
                 lst += '*'
             else:
                 lst += '$'
-            print(f'line {counter}: {lst}')
+            print(lst)
             counter += 1
             lst += ' '
-        for num2 in range(half_size, size):
-            # print(f'num2: {num2}, downsizer: {downsizer}')
-            print(f'line {counter}: {lst[:downsizer]}')
+        for num2 in range(size + 1, size * 2):
+            print(lst[:downsizer])
             counter += 1
             downsizer -= 2
 
-arrow(10)
+
+arrow(int(input('Arrow width: ')))
