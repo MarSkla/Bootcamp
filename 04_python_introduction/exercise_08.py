857be347abe918d912fa101d4bbc579c996c8353
MarSkla <marcin@skladanowski.net>
2021-11-03 12:58:54 +0100

    print info on string used to test functionality

--- 04_python_introduction/exercise_08.py
+++ 04_python_introduction/exercise_08.py
@@ -5,6 +5,8 @@ print()  # empty line for better readability
 test_string_1 = 'te$st te/st,'
 test_string_2 = '#Tset ts^!eT'
 
+print(f'Test for:\n\tstring no.1: "{test_string_1}"\n\tstring no.2: "'
+      f'{test_string_2}"\n')
 
 def palindromes(string_1, string_2):
     assert isinstance(string_1, str) and isinstance(string_2, str), \
