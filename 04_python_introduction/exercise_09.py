7c7554f7faa41f1869163ab579e778908df583ee
MarSkla <marcin@skladanowski.net>
2021-11-03 13:08:30 +0100

    print taks info for exercise 09

---
+++ 04_python_introduction/exercise_09.py
@@ -0,0 +1,4 @@
+print('\nexercise_09.py:\n\tuse nested loops to build function printing:',
+      '*', '* $', '* $ *', '* $ * $', '* $ * $ *', '* $ * $', '* $ *', '* $',
+      '*', sep='\n\t')
+print()  # empty line for better readability
