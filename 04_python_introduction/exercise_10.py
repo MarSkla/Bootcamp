d98a4e4aa9523786f710915c1d03fbadefa8fc0b
MarSkla <marcin@skladanowski.net>
2021-11-04 06:47:00 +0100

    add generator for both lists

--- 04_python_introduction/exercise_10.py
+++ 04_python_introduction/exercise_10.py
@@ -3,3 +3,10 @@ print('\nexercise_10.py:\n\tbuild a function that accepts two lists of '
       'in the second one, sorted in ascending order based on number of '
       'occurrences in the first list')
 print()  # empty line for better readability
+
+import random
+
+lst_a = [random.randint(0, 10) for lst_a_item in range(30)]
+lst_b = [random.randint(0, 5) for lst_b_item in range(15)]
+print(f'lst_a: {lst_a}')
+print(f'lst_b: {lst_b}')
