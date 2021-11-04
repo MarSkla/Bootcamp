34bee747e8ff438cf2dc5296009bcbf84d556f9f
MarSkla <marcin@skladanowski.net>
2021-11-04 06:46:11 +0100

    add task info

---
+++ 04_python_introduction/exercise_10.py
@@ -0,0 +1,5 @@
+print('\nexercise_10.py:\n\tbuild a function that accepts two lists of '
+      'integers and returns a list of the numbers in the first list and not '
+      'in the second one, sorted in ascending order based on number of '
+      'occurrences in the first list')
+print()  # empty line for better readability
