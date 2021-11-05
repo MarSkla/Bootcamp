bf81696c37569f86ec6a7173477273536e99f514
MarSkla <marcin@skladanowski.net>
2021-11-05 10:12:31 +0100

    add working code meeting task requirements

--- 04_python_introduction/exercise_13.py
+++ 04_python_introduction/exercise_13.py
@@ -1,3 +1,13 @@
-print('\nexercise_13.py:\n\tBuild a function that takes "name:height" '
-      'dictionary as an argument and return a list of names sorted by '
-      'height in descending order.', end='\n\n')
+print('\nexercise_13.py:\n\tBuild a function that takes dictionary mapping '
+      'names with height as an argument and returns a list of names sorted '
+      'by height in descending order.', end='\n\n')
+
+test_dict = {'John': 185, 'Emma': 167, 'Chewbacca': 260, 'Cristal': 173,
+             'Giovanni': 179}
+
+
+def dict_sort(dictionary):
+    result = sorted(dictionary, key=dictionary.get, reverse=True)
+    return result
+
+# print(dict_sort(test_dict))
