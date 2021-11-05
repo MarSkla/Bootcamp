0684eaf0861512d32935e351ebafb372f36365b2
MarSkla <marcin@skladanowski.net>
2021-11-05 10:37:44 +0100

    add info on task objectives

---
+++ 04_python_introduction/exercise_14.py
@@ -0,0 +1,3 @@
+print('\nexercise_14.py:\n\tBuild a function taking two arguments and '
+      'returning power of 1st argument(base) to 2nd argument (exponent). '
+      'Use recursion', end='\n\n')
