0fa6022bb1af7cc0ed2814f84f7ff4cba72967e5
MarSkla <marcin@skladanowski.net>
2021-11-05 09:30:26 +0100

    add info on task objectives

---
+++ 04_python_introduction/exercise_13.py
@@ -0,0 +1,3 @@
+print('\nexercise_13.py:\n\tBuild a function that takes "name:height" '
+      'dictionary as an argument and return a list of names sorted by '
+      'height in descending order.', end='\n\n')
