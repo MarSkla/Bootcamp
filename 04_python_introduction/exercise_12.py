180f07eb5eb7eec6123f70d447ea2f02d4dc44a8
MarSkla <marcin@skladanowski.net>
2021-11-05 08:42:30 +0100

    add more friendly 'better_entry()'

--- 04_python_introduction/exercise_12.py
+++ 04_python_introduction/exercise_12.py
@@ -3,9 +3,20 @@ print('\nexercise_12.py:\n\tBuild a function that takes two arguments (name, '
       'Add 5 entries at least.', end='\n\n')
 
 global_dict = {}
+global_dict_2 = {}
 
-def standard_entry(name, height):
-    global_dict[name] = height
+
+def standard_entry(name_txt, height):
+    global_dict[name_txt] = height
+
+
+def better_entry():
+    num_of_entries = int(input('How many entries would you like to add? : '))
+    for num in range(num_of_entries):
+        name_txt = input(f'Provide name no. {num + 1}: ').title()
+        height = int(input(f'Provide height no. {num + 1}: '))
+        global_dict_2[name_txt] = height
+
 
 standard_entry('John', 185)
 standard_entry('Emma', 167)
@@ -13,5 +24,9 @@ standard_entry('Chewbacca', 260)
 standard_entry('Cristal', 173)
 standard_entry('Giovanni', 179)
 
-print(global_dict)
+better_entry()
 
+print('\nGlobal dictionary for "standard_entry(name, height)"',
+      *global_dict.items(), sep='\n\t')
+print('\nGlobal dictionary for "better_entry()"',
+      *global_dict_2.items(), sep='\n\t')
