e5d0733dab6d08d784aefd48ef8b6e1ce4a7b8f7
MarSkla <marcin@skladanowski.net>
2021-11-05 10:51:18 +0100

    add my_power() in line with  task requirements

--- 04_python_introduction/exercise_14.py
+++ 04_python_introduction/exercise_14.py
@@ -1,3 +1,9 @@
 print('\nexercise_14.py:\n\tBuild a function taking two arguments and '
       'returning power of 1st argument(base) to 2nd argument (exponent). '
       'Use recursion', end='\n\n')
+
+def my_power(x, y):
+    if y == 0:
+        return 1
+    else:
+        return x * my_power(x, y - 1)
