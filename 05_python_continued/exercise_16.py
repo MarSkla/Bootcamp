02d0158f356d844714ef38503c77aa6e5aaed7bd
MarSkla <marcin@skladanowski.net>
2021-11-15 15:30:36 +0100

    add initial code to exercise_16.py

--- 05_python_continued/exercise_16.py
+++ 05_python_continued/exercise_16.py
@@ -1,3 +1,22 @@
 print('\nexercise_16.py:\n\tBuild Class Diary and implement methods to '
-      'read and write content which will be stored in txt file.,
+      'read and write content which will be stored in txt file.',
       end='\n\n')
+
+
+class Diary:
+
+    def __init__(self, file):
+        self.file = file
+
+    def check(self):
+        print('Provide file name:\t\t', self.file)
+
+    def add_text(self):
+        a = open(self.file, 'at')
+        a.write(input('Please share your thoughts:'))
+        a.write('\n')
+        a.close
+
+    def read_diary(self):
+        a = open(self.file, 'r')
+        print(a.read())
