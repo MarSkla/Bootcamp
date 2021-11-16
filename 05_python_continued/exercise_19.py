53ba79f988f5565e90d7b7948c80dee7d4952f8d
MarSkla <marcin@skladanowski.net>
2021-11-16 12:46:38 +0100

    add exercise_19.py with task info inside

---
+++ 05_python_continued/exercise_19.py
@@ -0,0 +1,4 @@
+print("\nexercise_19.py:\n\tBuild Class 'Figure' with two methods for "
+      "calculation of area and perimeter. Create subclasses representing "
+      "triangle and rectangle implementing appropriate method", end="\n\n")
+
