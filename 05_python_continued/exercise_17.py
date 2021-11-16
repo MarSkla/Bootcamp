94c0f76a7e6bcf25e65aab61afe4cd7b2984b7a9
MarSkla <marcin@skladanowski.net>
2021-11-16 10:06:10 +0100

    add initial code - under construction phase

--- 05_python_continued/exercise_17.py
+++ 05_python_continued/exercise_17.py
@@ -2,3 +2,21 @@ print('\nexercise_17.py:\n\tBuild Class "Triangle" (accepting 3 arguments being'
       'triangle angles) that  implement "check_angles" method adding all '
       'arguments and returning info whether arguments sums to triangle or not ',
       end='\n\n')
+
+class Triangle():
+
+    def __init__(self, ang_1, ang_2, ang_3):
+        self.ang_1 = ang_1
+        self.ang_2 = ang_2
+        self.ang_3 = ang_3
+
+    def check_angles(self):
+        result = self.ang_1 + self.ang_2 + self.ang_3
+        start_statement = "Sum of all angles equals to: "
+        answer = ''
+        if result == 180:
+            answer = "They may constitute triangle"
+            print(start_statement, result)
+        else:
+            answer = "These tree angles will not create triangle"
+        print(answer)
