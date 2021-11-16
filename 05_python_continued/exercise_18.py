b584b55d197766cd8fcb7d2ce62a33e02f8409c4
MarSkla <marcin@skladanowski.net>
2021-11-16 10:46:23 +0100

    add code meeting task objectives

--- 05_python_continued/exercise_18.py
+++ 05_python_continued/exercise_18.py
@@ -1,3 +1,17 @@
-print('\nexercise_18.py:\n\tBuild Class "Song" (accepting a list of strings '
-      'representing single verse of the song) that consist "sing" method '
-      'that print full text', end='\n\n')
+print("\nexercise_18.py:\n\tBuild Class 'Song' (accepting a list of strings "
+      "representing single verse of the song) that consist 'sing' method "
+      "that print full text", end="\n\n")
+
+
+class Song:
+
+    def __init__(self, list):
+        self.list = list
+
+    def sing(self):
+        print(*self.list, sep="\n")
+        
+        
+song1 = Song(["jakis tekst", "jakiś tekst 2", "jakiś tekst 3"])
+
+song1.sing()
