53669ecb8d4ab69f9ecb1598a4599638922772a3
MarSkla <marcin@skladanowski.net>
2021-11-16 10:38:07 +0100

    add new file for exercise_18, task info included

---
+++ 05_python_continued/exercise_18.py
@@ -0,0 +1,3 @@
+print('\nexercise_18.py:\n\tBuild Class "Song" (accepting a list of strings '
+      'representing single verse of the song) that consist "sing" method '
+      'that print full text', end='\n\n')
