f36250e52b2db3c795ef8625390f75f100279044
MarSkla <marcin@skladanowski.net>
2021-11-16 09:57:24 +0100

    add new file exercise_17.py with task info

---
+++ 05_python_continued/exercise_17.py
@@ -0,0 +1,4 @@
+print('\nexercise_17.py:\n\tBuild Class "Triangle" (accepting 3 arguments being'
+      'triangle angles) that  implement "check_angles" method adding all '
+      'arguments and returning info whether arguments sums to triangle or not ',
+      end='\n\n')
