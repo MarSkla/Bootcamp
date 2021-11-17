25b33ee20fbed141018866db0d8ca607d49bfc46
MarSkla <marcin@skladanowski.net>
2021-11-17 09:28:04 +0100

    add refactored code

--- 05_python_continued/exercise_19.py
+++ 05_python_continued/exercise_19.py
@@ -7,43 +7,52 @@ class Figure:
 
     def __init__(self, values):
         assert isinstance(values, (list, tuple))
-
         self.values = values
-        self.height = self.values[1]
-        self.base = self.values[0]
+        self.height = self.values[0]
+        self.base = self.values[1]
 
-        self.area = 0
-        self.perimeter = 0
+        # self.area = 0
+        # self.perimeter = 0
 
     def area(self):
         return NoteImplemented
 
     def perimeter(self):
         self.perimeter = sum(self.values)
+        print("Perimeter of the figure is: ", self.perimeter)
 
 
 class Rectangle(Figure):
 
     def __init__(self, values):
         super().__init__(values)
-
+    # not necessary but more effective - only method called not entire class1
 
     def area(self):
         self.area = self.base * self.height
+        print("Area of the fig is: ", self.area)
 
 
 class Triangle(Figure):
 
     def __init__(self, values):
         super().__init__(values)
-
+    # not necessary but more effective - only method called not entire class
 
     def area(self):
         self.area = self.base * self.height / 2
+        print("Area of the fig is: ", self.area)
+
+    def perimeter(self):
+        self.perimeter = sum(self.values[1:])
+        print("Triangle perimeter is :", self.perimeter)
 
 
 fig_1 = Rectangle((2, 4))
+fig_2 = Triangle((1.5, 4, 2, 5))
 
+fig_1.perimeter()
+fig_1.area()
 
-
-
+fig_2.perimeter()
+fig_2.area()
