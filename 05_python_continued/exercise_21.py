851f37067008e2e7ceb0702212ae07b9e9fccfaf
MarSkla <marcin@skladanowski.net>
2021-11-22 15:22:50 +0100

    add method vector_length

--- 05_python_continued/exercise_21.py
+++ 05_python_continued/exercise_21.py
@@ -5,6 +5,8 @@ print("\nexercise_21.py:\n\tBuild Class 'Polygon' where constructor "
       "poind of the previous input. 'Polygon' Class should also include "
       "method to calculate perimeter of the polygon", end="\n\n")
 
+from math import hypot
+
 
 class Polygon:
 
@@ -16,6 +18,18 @@ class Polygon:
             self.vector = [int(x) for x in self.vector.split(",")]
             self.coordinates.append(self.vector)
 
+    def vector_length(self):
+        vector_len = []
+        for el in self.coordinates:
+            result = hypot((el[2]-el[0]), (el[3]-el[1]))
+            vector_len.append(round(result, 2))
+        print("Vectors lengths (math.hypo): ", vector_len)
+
+    # def perimeter_cal(self):
+    #         self.vector_length()
+    #         perimeter = sum(vector_length.vector_len)
+    #         print("Perimeter: ", perimeter)
+    #
     # def print_in(self):
     #     print("vector: ", self.vector)
     #     print("coordinates: ", self.coordinates)
@@ -24,4 +38,6 @@ class Polygon:
 
 figure = Polygon()
 
-figure.print_in()
+# figure.print_in()
+
+figure.vector_length()
