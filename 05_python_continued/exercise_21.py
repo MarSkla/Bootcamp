bc095e650a79420473260f16e08869f4f5c33580
MarSkla <marcin@skladanowski.net>
2021-11-22 14:05:21 +0100

    add new exercise_21 file with task info

---
+++ 05_python_continued/exercise_21.py
@@ -0,0 +1,6 @@
+print("\nexercise_21.py:\n\tBuild Class 'Polygon' where constructor "
+      "ask for coordinates of vector (start + end point) being sides "
+      "of a polygon. 'Polygon'Class should check if the input is "
+      "correct, i.e. start point of the next side is the same as end "
+      "poind of the previous input. 'Polygon' Class should also include "
+      "method to calculate perimeter of the polygon", end="\n\n")
