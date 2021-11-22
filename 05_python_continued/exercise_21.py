af346825be2c73e5ea0d68b9f887eb6a7a958824
MarSkla <marcin@skladanowski.net>
2021-11-22 19:27:21 +0100

    add 'perimeter_cal' method

--- 05_python_continued/exercise_21.py
+++ 05_python_continued/exercise_21.py
@@ -1,8 +1,8 @@
 print("\nexercise_21.py:\n\tBuild Class 'Polygon' where constructor "
       "ask for coordinates of vector (start + end point) being sides "
-      "of a polygon. 'Polygon'Class should check if the input is "
+      "of a polygon. 'Polygon' Class should check if the input is "
       "correct, i.e. start point of the next side is the same as end "
-      "poind of the previous input. 'Polygon' Class should also include "
+      "point of the previous input. 'Polygon' Class should also include "
       "method to calculate perimeter of the polygon", end="\n\n")
 
 from math import hypot
@@ -18,18 +18,17 @@ class Polygon:
             self.vector = [int(x) for x in self.vector.split(",")]
             self.coordinates.append(self.vector)
 
-    def vector_length(self):
+    def vectors_lengths(self):
         vector_len = []
         for el in self.coordinates:
-            result = hypot((el[2]-el[0]), (el[3]-el[1]))
+            result = hypot((el[2] - el[0]), (el[3] - el[1]))
             vector_len.append(round(result, 2))
-        print("Vectors lengths (math.hypo): ", vector_len)
+        return vector_len
 
-    # def perimeter_cal(self):
-    #         self.vector_length()
-    #         perimeter = sum(vector_length.vector_len)
-    #         print("Perimeter: ", perimeter)
-    #
+    def perimeter_cal(self):
+        perimeter = self.vectors_lengths()
+        print("Perimeter: ", sum(perimeter))
+
     # def print_in(self):
     #     print("vector: ", self.vector)
     #     print("coordinates: ", self.coordinates)
@@ -40,4 +39,6 @@ figure = Polygon()
 
 # figure.print_in()
 
-figure.vector_length()
+print(figure.vectors_lengths())
+
+figure.perimeter_cal()
