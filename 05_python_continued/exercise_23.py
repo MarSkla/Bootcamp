b1e12b6eae085e91d648f84be218295120c5c3e7
MarSkla <marcin@skladanowski.net>
2021-11-26 11:27:36 +0100

    add file for ex_23

---
+++ 05_python_continued/exercise_23.py
@@ -0,0 +1,5 @@
+print("\nexercise_22.py:\n\tBuild base class Animal and two subclasses 'Dog' "
+      "and 'Cat'. Constructor must consist a variable describing type of an "
+      "animal. Dog & Cat classes must consist make_sound method. Check "
+      "whether instances of Dog & Cat classes will return proper greeting "
+      "message.", end="\n\n")
