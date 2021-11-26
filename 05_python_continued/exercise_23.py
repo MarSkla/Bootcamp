5cb6ef7c0d0cce577519c3af6c1aaa8a9557e3dd
MarSkla <marcin@skladanowski.net>
2021-11-26 11:38:13 +0100

    add solution to ex_23

--- 05_python_continued/exercise_23.py
+++ 05_python_continued/exercise_23.py
@@ -1,5 +1,44 @@
 print("\nexercise_22.py:\n\tBuild base class Animal and two subclasses 'Dog' "
-      "and 'Cat'. Constructor must consist a variable describing type of an "
-      "animal. Dog & Cat classes must consist make_sound method. Check "
+      "and 'Cat'. Dog&Cat Constructors must consist a variable describing type "
+      "of an animal. Dog & Cat classes must consist make_sound method. Check "
       "whether instances of Dog & Cat classes will return proper greeting "
       "message.", end="\n\n")
+
+
+class Animal:  
+    
+    def __init__(self):
+        pass
+
+    def greeting(self):
+        print(f'I\'m a {self.animal_type}. {self.make_sound()}')
+
+
+class Dog(Animal):
+
+    def __init__(self, animal_sound):
+        super().__init__()
+        self.animal_type = animal_sound
+
+    @staticmethod
+    def make_sound():
+        return 'Woof!'
+
+
+class Cat(Animal):
+
+    def __init__(self, animal_sound):
+        super().__init__()
+        self.animal_type = animal_sound
+
+    @staticmethod
+    def make_sound():
+        return 'Meow....'
+
+
+tequila = Dog('dog')
+tequila.greeting()
+print()
+
+gin = Cat('cat')
+gin.greeting()
