27d59661a90e97fc3634ad75c5a87550625a28e7
MarSkla <marcin@skladanowski.net>
2021-11-26 09:48:04 +0100

    add new file for ex_22(abstract class)

---
+++ 05_python_continued/exercise_22.py
@@ -0,0 +1,5 @@
+print("\nexercise_22.py:\n\tBuild abstract base class Animal. Constructor is "
+      "going to accept an argument representing name of an animal. Class must "
+      "have 2 methods: 1st 'sound' is going to be abstract. Second 'welcome' "
+      "has to print string build as value returned by 'sound' extended by "
+      "name of the animal and type of the animal.", end="\n\n")
