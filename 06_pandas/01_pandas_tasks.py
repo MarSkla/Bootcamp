8ae0df1efcc6bff483849c23cbd7d5e8873edbad
MarSkla <marcin@skladanowski.net>
2021-11-29 18:50:28 +0100

    add solution to task_02

--- 06_pandas/01_pandas_tasks.py
+++ 06_pandas/01_pandas_tasks.py
@@ -2,7 +2,22 @@ import pandas as pd
 
     # exercise_01.py:Load provided dataset including following columns: imię,
     # nazwisko, rok urodzenia, miesiac urodzenia, dzień urodzenia, miejsce
-    # urodzenia, miasto oddziału, departament, miesiac rozpoczęcia pracy,
-    # dzień rozpoczęcia pracy, doswiadczenie (w latach)
+    # urodzenia, miasto oddziału, departament, rok rozpoczecia pracy, miesiac
+    # rozpoczęcia pracy, dzień rozpoczęcia pracy, doswiadczenie (w latach).
 
-data = pd.read_csv('dataset_pandas_pd.csv')
+data = pd.read_csv('dataset_pandas_pd.csv', header=None,
+                   names=['name', 'surname', 'year_ob', 'month_ob', 'day_ob',
+                          'city_ob', 'branch_city', 'department',
+                          'year_work_start', 'month_work_start',
+                          'day_work_start', 'years_experience'])
+# print(data)
+
+    # exercise_02.py: Which surname is most common?
+
+most_common_surname = data.value_counts('surname')
+# print(most_common_surname)
+
+    # ans: 'Abacki - 7 times'
+
+
+
