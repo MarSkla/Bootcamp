4de4ff58c1e3a9e12334c67f3b40ec136af9573f
MarSkla <marcin@skladanowski.net>
2021-11-29 20:27:41 +0100

    add solution to task_03

--- 06_pandas/01_pandas_tasks.py
+++ 06_pandas/01_pandas_tasks.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
-    # exercise_01.py:Load provided dataset including following columns: imię,
-    # nazwisko, rok urodzenia, miesiac urodzenia, dzień urodzenia, miejsce
-    # urodzenia, miasto oddziału, departament, rok rozpoczecia pracy, miesiac
-    # rozpoczęcia pracy, dzień rozpoczęcia pracy, doswiadczenie (w latach).
+# exercise_01:
+# Load provided dataset including following columns: imię,
+# nazwisko, rok urodzenia, miesiac urodzenia, dzień urodzenia, miejsce
+# urodzenia, miasto oddziału, departament, rok rozpoczecia pracy, miesiac
+# rozpoczęcia pracy, dzień rozpoczęcia pracy, doswiadczenie (w latach).
 
 data = pd.read_csv('dataset_pandas_pd.csv', header=None,
                    names=['name', 'surname', 'year_ob', 'month_ob', 'day_ob',
@@ -12,12 +13,19 @@ data = pd.read_csv('dataset_pandas_pd.csv', header=None,
                           'day_work_start', 'years_experience'])
 # print(data)
 
-    # exercise_02.py: Which surname is most common?
+# exercise_02:
+# Which surname is most common?
 
 most_common_surname = data.value_counts('surname')
 # print(most_common_surname)
 
-    # ans: 'Abacki - 7 times'
+# ans: 'Abacki - 7 times'
+
+# exercise_03:
+# add 'sex' column after 'surname' representing sex of the person
+
+data.insert(2, 'sex', data.name.apply(lambda x: 'female' if x[-1] == 'a' else
+                                                'male'))
 
 
-
+print(data.iloc[:, :5])
