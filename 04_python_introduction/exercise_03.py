4f7f3b085b27f8e4b96749a81daed58285d01534
MarSkla <marcin@skladanowski.net>
2022-01-12 10:25:54 +0100

    add folder with Final Project for Data Science Bootcamp - originally done on March, 28, 2021

--- 04_python_introduction/exercise_03.py
+++ 04_python_introduction/exercise_03.py
@@ -1,38 +1,42 @@
-print('\nexercise_03.py:\n\tsort list elements in descending order')
-# Python has standard sorted() function and .sort() method (lists only)
-# however I do intend to build my own function with same result as sorted()
-# sorting in order set by ASCII table
+# print('\nexercise_03.py:\n\tsort list elements in descending order')
+# # Python has standard sorted() function and .sort() method (lists only)
+# # however I do intend to build my own function with same result as sorted()
+# # sorting in order set by ASCII table
+#
+# print()  # empty line for better readability
+#
+#
+# def sorting(lst, direction='asc'):
+#     for first_index in range(len(lst)):
+#         for second_index in range(first_index):
+#             if direction == 'asc':
+#                 if lst[second_index] > lst[first_index]:
+#                     lst[first_index], lst[second_index] = lst[second_index], \
+#                                                           lst[first_index]
+#             if direction == 'dsc':
+#                 if lst[second_index] < lst[first_index]:
+#                     lst[first_index], lst[second_index] = lst[second_index], \
+#                                                           lst[first_index]
+#     return lst
+#
+#
+# test_case = [[[1, 3, -4, 2, 4, 5]], [['c', 'e', 'g', 'a', 'f']],
+#              [['c', 'E', 'g', 'A', 'F']], [[1, 3, -4, 2, 4, 5], 'dsc'],
+#              [['c', 'e', 'g', 'a', 'f'], 'dsc'], [['c', 'E', 'g', 'A', 'F'],
+#                                                   'dsc']]
+#
+# test_no = 1
+#
+# for case_lst in test_case:
+#     if len(case_lst) == 1:
+#         print(f'test no. {test_no} for list:\n\t {case_lst}')
+#         print('result:\n\t', sorting(case_lst), '\n')
+#         test_no += 1
+#     elif len(case_lst) == 2:
+#         print(f'test no. {test_no} for list:\n\t {case_lst}')
+#         print('result:\n\t', sorting(case_lst[0], case_lst[1]), '\n')
+#         test_no += 1
 
-print()  # empty line for better readability
+testArray = [7, 1, 8, 0, 8, 2, 9, 4]
 
-
-def sorting(lst, direction='asc'):
-    for first_index in range(len(lst)):
-        for second_index in range(first_index):
-            if direction == 'asc':
-                if lst[second_index] > lst[first_index]:
-                    lst[first_index], lst[second_index] = lst[second_index], \
-                                                          lst[first_index]
-            if direction == 'dsc':
-                if lst[second_index] < lst[first_index]:
-                    lst[first_index], lst[second_index] = lst[second_index], \
-                                                          lst[first_index]
-    return lst
-
-
-test_case = [[[1, 3, -4, 2, 4, 5]], [['c', 'e', 'g', 'a', 'f']],
-             [['c', 'E', 'g', 'A', 'F']], [[1, 3, -4, 2, 4, 5], 'dsc'],
-             [['c', 'e', 'g', 'a', 'f'], 'dsc'], [['c', 'E', 'g', 'A', 'F'],
-                                                  'dsc']]
-
-test_no = 1
-
-for case_lst in test_case:
-    if len(case_lst) == 1:
-        print(f'test no. {test_no} for list:\n\t {case_lst}')
-        print('result:\n\t', sorting(case_lst), '\n')
-        test_no += 1
-    elif len(case_lst) == 2:
-        print(f'test no. {test_no} for list:\n\t {case_lst}')
-        print('result:\n\t', sorting(case_lst[0], case_lst[1]), '\n')
-        test_no += 1
+for (i=0; i)
